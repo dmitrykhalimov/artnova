45f5c2a9543aa49edbd161fd5ed2d1d0849ba7e2
Dmitry <dmitry.khalimov@gmail.com>
2021-05-22 20:41:18 +0300

    Правит комментарии

--- source/mail.php
+++ source/mail.php
@@ -12,27 +12,19 @@ if (!isset($key)) {
     echo('Unauthorized');
     die;
 }
-//$email = $_POST['user_email'];
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+$mail->isSMTP();
+$mail->Host = 'smtp.mail.ru';
+$mail->SMTPAuth = true;
+$mail->Username = '';
+$mail->Password = '';
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = ''; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = ''; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
-
-$mail->setFrom(''); // от кого будет уходить письмо?
-$mail->addAddress('');     // Кому будет уходить письмо
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
+$mail->setFrom('');
+$mail->addAddress('');
 $mail->addCC('');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
+$mail->isHTML(true);
 
 $mail->Subject = 'Заявка с сайта Арт Нова';
 $mail->Body    = '' .$name . ' оставил заявку на консультацию, его телефон ' .$phone;
